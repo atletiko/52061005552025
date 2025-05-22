a6bffce6d63ea5ec27b0953aa17ee38a2088300e
atletiko <atletiko@gmail.com>
2025-05-22 15:48:00 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\DosenController ;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\BlogController ;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dosen',[DosenController::class,'index']);
+Route::get('welcome',[DosenController::class,'welcome']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class,'index']);
+Route::get('/formulir', [PegawaiController::class,'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class,'home']);
+Route::get('/blog/tentang', [BlogController::class,'tentang'] );
+Route::get('/blog/kontak', [BlogController::class,'kontak']);
